f8f8fc087e9cdf76c480a137cf5d3b1157f97cd6
unknown <khyogeun_@naver.com>
2022-02-15 18:39:44 +0900

    [#3] Face-Parsing

--- Face-parsing/face_parsing.py
+++ Face-parsing/face_parsing.py
@@ -6,6 +6,7 @@ from model import BiSeNet
 
 import torch
 
+import sys
 import os
 import os.path as osp
 import numpy as np
@@ -23,7 +24,7 @@ def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_res
                    [0, 85, 255], [0, 170, 255],
                    [255, 255, 0], [255, 255, 85], [255, 255, 170],
                    [255, 0, 255], [255, 85, 255], [255, 170, 255],
-                   [0, 255, 255], [85, 255, 255], [170, 255, 255]]
+                    [0, 255, 255], [85, 255, 255], [170, 255, 255]]
 
     im = np.array(im)
     vis_im = im.copy().astype(np.uint8)
@@ -43,48 +44,28 @@ def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_res
 
     # Save result or not
     if save_im:
-        cv2.imwrite(save_path[:-4] +'.png', vis_parsing_anno)
-        cv2.imwrite(save_path, vis_im, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+        cv2.imwrite(save_path, vis_parsing_anno)
 
-    # return vis_im
-
-def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth'):
-
-    if not os.path.exists(respth):
-        os.makedirs(respth)
+def evaluate(respth='./res/res000.jpg', dspth='./src/src000.jpg', cp='./res/cp/79999_iter.pth'):
 
     n_classes = 19
     net = BiSeNet(n_classes=n_classes)
     net.cuda()
-    save_pth = osp.join('res/cp', cp)
-    net.load_state_dict(torch.load(save_pth))
+    net.load_state_dict(torch.load(cp))
     net.eval()
 
     to_tensor = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
+
     with torch.no_grad():
-        for image_path in os.listdir(dspth):
-            img = Image.open(osp.join(dspth, image_path))
-            image = img.resize((512, 512), Image.BILINEAR)
-            img = to_tensor(image)
-            img = torch.unsqueeze(img, 0)
-            img = img.cuda()
-            out = net(img)[0]
-            parsing = out.squeeze(0).cpu().numpy().argmax(0)
-            # print(parsing)
-            print(np.unique(parsing))
-
-            vis_parsing_maps(image, parsing, stride=1, save_im=True, save_path=osp.join(respth, image_path))
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    evaluate(dspth='/home/zll/data/CelebAMask-HQ/test-img', cp='79999_iter.pth')
-
-
+        img = Image.open(dspth)
+        image = img.resize((512, 512), Image.BILINEAR)
+        img = to_tensor(image)
+        img = torch.unsqueeze(img, 0)
+        img = img.cuda()
+        out = net(img)[0]
+        parsing = out.squeeze(0).cpu().numpy().argmax(0)
+        
+        vis_parsing_maps(image, parsing, stride=1, save_im=True, save_path=respth)
