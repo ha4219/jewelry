92f1ad6c7166a0f3b903a93105abc13e70871691
ha4219 <ha4219@naver.com>
2022-02-23 17:48:58 +0900

    [ha4219] server check

--- server/app.py
+++ server/app.py
@@ -56,4 +56,7 @@ def uploader_file():
         return fa_dst_path, fp_dst_path
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+            host="0.0.0.0",
+            port = "5000"
+            )
